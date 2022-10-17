e5fe6d8c6f524d5ea692f2032a50b951cfd4149b
Charlie Le <20309750+CharlieQLe@users.noreply.github.com>
2022-10-17 15:55:04 -0400

    Fixed open wallpaper button

--- src/prefs.js
+++ src/prefs.js
@@ -78,11 +78,10 @@ function fillPreferencesWindow(window) {
             // Details
             const detailsRow = new Adw.ActionRow();
             if (wallpaperData.path.endsWith('.xml')) detailsRow.add_prefix(_createButton("Open XML", () => Gtk.show_uri(window, convertPathToURI(wallpaperData.path), Gdk.CURRENT_TIME)));
-            if (wallpaperData.light === wallpaperData.dark) detailsRow.add_suffix(_createButton("Open Wallpaper", () => Gtk.show_uri(window, wallpaperData.light, Gdk.CURRENT_TIME)));
-            else {
-                detailsRow.add_suffix(_createButton("Open Light Wallpaper", () => Gtk.show_uri(window, wallpaperData.light, Gdk.CURRENT_TIME)));
-                detailsRow.add_suffix(_createButton("Open Dark Wallpaper", () => Gtk.show_uri(window, wallpaperData.dark, Gdk.CURRENT_TIME)));
-            }
+            if (wallpaperData.isThemed()) {
+                detailsRow.add_suffix(_createButton("Open Light Wallpaper", () => Gtk.show_uri(window, wallpaperData.lightUri, Gdk.CURRENT_TIME)));
+                detailsRow.add_suffix(_createButton("Open Dark Wallpaper", () => Gtk.show_uri(window, wallpaperData.darkUri, Gdk.CURRENT_TIME)));
+            } else detailsRow.add_suffix(_createButton("Open Wallpaper", () => Gtk.show_uri(window, wallpaperData.lightUri, Gdk.CURRENT_TIME)));
             wallpaperRow.add_row(detailsRow);
 
             // Add row to widget
